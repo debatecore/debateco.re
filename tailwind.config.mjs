96a2412f30dce786cf33856f54f0fd0b4754ee50
jakubmanczak <jakub@manczak.net>
2024-03-28 13:48:45 +0100

    sparkle up the landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        compsci: `url("/compsci-bg.svg")`,
+      },
+      animation: {
+        pan: "pan_keyframes 100s linear infinite",
+        slidein: "slidein_keyframes .5s ease-in-out",
+      },
+      keyframes: {
+        pan_keyframes: {
+          "0%": { "background-position": "0% 0%" },
+          "100%": { "background-position": "0% 100%" },
+        },
+        slidein_keyframes: {
+          "0%": {
+            opacity: "0%",
+            translate: "0% 25%",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
